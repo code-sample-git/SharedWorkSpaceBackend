e902c16f616d6266cfaac5f279412681b4d189cb
Michael <8717608+code-sample-git@users.noreply.github.com>
2024-04-11 11:45:24 -0600

    added body size limit

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 require('dotenv').config();
 const express = require('express');
+const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const cors = require('cors');
 
@@ -12,7 +13,9 @@ const app = express();
 
 // Middlewares
 app.use(cors());
-app.use(express.json()); // for parsing application/json
+app.use(bodyParser.json({ limit: '20mb' }));
+app.use(bodyParser.urlencoded({ extended: true, limit: '20mb' }));
+
 
 // Connect to MongoDB
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
